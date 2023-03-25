b65765204848a574ba3b27c5ef4ab5873ae9fdf1
ABViney <vineyadrian@gmail.com>
2023-03-25 19:38:04 -0400

    Refactor, failed inline styling

--- src/app.ts
+++ src/app.ts
@@ -63,28 +63,6 @@ export class App {
     graphics.container.eventMode = 'static';
     bindListeners(graphics);
 
-    // Fancy button (Red)
-    const fancybutton: NamedContainer = {
-      name: 'FancyButton',
-      container: new FancyButton({
-        defaultView: new Sprite(
-          renderer.generateTexture(new Graphics().beginFill(0xff0000) .drawRect(0,0,width/4,height/4).endFill())
-        )
-      })
-    };;
-    // This signal will properly evoke if the button is clicked.
-    (fancybutton.container as FancyButton).onPress.connect(() => console.log(`${fancybutton.name} has been pressed`));
-    bindListeners(fancybutton);
-
-    // Child element (Green) of the FancyButton aren't overshadowed.
-    const graphics_child:NamedContainer = {
-      name: 'Graphics_child',
-      container: new Graphics().beginFill(0x00ff00).drawRect(0,0,width/8,height/8).endFill()
-    };
-    graphics_child.container.eventMode = 'static';
-    bindListeners(graphics_child);
-    fancybutton.container.addChild(graphics_child.container);
-
     // Switcher (Red/Green)
     const switcher: NamedContainer = {
       name: 'Switcher',
@@ -95,27 +73,15 @@ export class App {
       )
     };
     bindListeners(switcher);
-    switcher.container.x = 300;
+    switcher.container.x = view.width - switcher.container.width;
+    console.log(view, stage)
 
     // Adding graphics (Blue) to stage.
     stage.addChild(graphics.container);
 
-    // Adding fancybutton (red) to stage
     /**
-     * If the FancyButton is rendered it will, for some reason, 
-     * encapsulate the DOM and steal all the 'pointermove' events
-     * such that no other containers can propogate them.
-     * 
-     * It will fire a 'pointerout' event when the cursor leaves the stage,
-     * however it will immediately fire a 'pointerover' event at that boundary
-     */
-    // stage.addChild(fancybutton.container); // Uncomment me!
-
-    /**
-     * The Switcher doesn't ensnare move events, but the hover event is only
-     * fired if the pointer enters its container from outside the DOM. It will
-     * immediately fire the 'pointerout' event, and cannot be initiated from
-     * within the DOM.
+     * The Switcher object's 'onHover' signal can be triggered by having its container be in the 
+     * first intersection between the pointer and the stage.
      */
     stage.addChild(switcher.container); // Uncomment me!
   }
